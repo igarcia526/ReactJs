5f2faf9b5cd2e6694b4ccb6dcfa0325d6a42ad56
Isaac <garciai0526@hotmail.com>
2024-07-24 17:20:17 -0400

    added component with api call as well as axios dependecy

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import StringInput from './StringInput';
+import RandomUser from './RandomUser';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -17,6 +18,7 @@ function App() {
 
     <div className='main'>
       <StringInput />
+      <RandomUser />
     </div>
 
     </>
